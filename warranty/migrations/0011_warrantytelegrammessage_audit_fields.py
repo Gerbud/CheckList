f75fa66031f25f582325d354736ebac1019bfde0
Buf <sasha@gerbud.ru>
2026-08-20 11:54:16 +0300

    Use utf8mb4 for Telegram audit text

--- warranty/migrations/0011_warrantytelegrammessage_audit_fields.py
+++ warranty/migrations/0011_warrantytelegrammessage_audit_fields.py
@@ -6,7 +6,18 @@ def copy_original_text(apps, schema_editor):
     Message.objects.filter(original_text='').update(original_text=models.F('text'))
 
 
+def use_utf8mb4_for_original_text(apps, schema_editor):
+    if schema_editor.connection.vendor != 'mysql':
+        return
+    schema_editor.execute(
+        'ALTER TABLE warranty_warrantytelegrammessage '
+        'MODIFY original_text LONGTEXT CHARACTER SET utf8mb4 '
+        'COLLATE utf8mb4_unicode_ci NOT NULL'
+    )
+
+
 class Migration(migrations.Migration):
+    atomic = False
     dependencies = [('warranty', '0010_warrantytelegrammessage_text_utf8mb4')]
     operations = [
         migrations.AddField(
@@ -19,5 +30,6 @@ class Migration(migrations.Migration):
             name='edited_at',
             field=models.DateTimeField(blank=True, null=True, verbose_name='изменено в Telegram'),
         ),
+        migrations.RunPython(use_utf8mb4_for_original_text, migrations.RunPython.noop),
         migrations.RunPython(copy_original_text, migrations.RunPython.noop),
     ]
